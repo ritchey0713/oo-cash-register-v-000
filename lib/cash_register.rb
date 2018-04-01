261fb7ca90d900320b065ad0b6368bb67ef00e6b
David Ritchey <ritchey0713@gmail.com>
2018-04-01 23:12:27 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,15 @@
 class CashRegister 
   
+  attr_accessor :total, :discount 
+  
+  def initialize(discount = 0)
+     @total = 0
+     @discount = discount
+     total
+  end 
+  
+  def add_item(item, price,quantity = 1)
+    total_quantity = price*quantity
+    @total +=total_quantity
+end
 end
